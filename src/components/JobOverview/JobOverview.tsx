2c71a5cda4b9588441264b032a85a1301465749e
Lavendelchen <lavendel02sh@gmail.com>
2024-09-29 20:19:09 +0200

    start filter selects, remove eslint

--- src/components/JobOverview/JobOverview.tsx
+++ src/components/JobOverview/JobOverview.tsx
@@ -1,9 +1,31 @@
 "use client";
 
 import styles from "./JobOverview.module.css";
+import { forwardRef } from "react";
 import { useQuery } from "@apollo/client";
 import { InitialJobsQuery, InitialJobsQueryVariables } from "@/types/graphql";
 import { INITIAL_JOBS } from "@/lib/query";
+import { Select } from "@mui/base/Select";
+import { Option } from "@mui/base/Option";
+
+type SelectRootSlotProps<TValue, Multiple> = {
+  ownerState: any;
+  children?: React.ReactNode;
+};
+
+const Button = forwardRef<HTMLButtonElement, SelectRootSlotProps<any, any>>(
+  function Button(props, ref) {
+    const { ownerState, ...other } = props;
+    return (
+      <button type="button" {...other} ref={ref}>
+        {other.children}
+        <span className="material-symbols-outlined"> 
+          keyboard_arrow_down
+        </span>
+      </button>
+    );
+  }
+);
 
 export default function JobOverview() {
   const initialJobsVariables = {
@@ -33,13 +55,37 @@ export default function JobOverview() {
     console.dir(error, { depth: null, color: true})
 
   if (data?.jobCollection)
-    jobCount = data.jobCollection.total
+    jobCount = data.jobCollection.total;
 
 
   return (
     <div className={styles.jobOverview}>
       <h5 className={styles.subHeading}>{jobCount} offene Stellen bei CreditPlus</h5>
       <h1 className={styles.heading}>Hier beginnt deine Zukunft</h1>
+      <div className={styles.filters}>
+        {Array.from({ length: 3 }, (_, index) => (
+          <Select
+            key={index}
+            className={styles.select}
+            slots={{
+              root: Button
+            }}
+            slotProps={{
+              popup: { className: styles.selectPopup },
+              listbox: { className: styles.selectListbox },
+            }}
+            value="Hello"
+            onChange={(event, newValue) => {}}
+          >
+            <Option className={styles.selectOption} key="Hello" value="Hello">
+              Hello
+            </Option>
+            <Option className={styles.selectOption} key="Hello" value="Hello">
+              Hello
+            </Option>
+          </Select>
+        ))}
+      </div>
     </div>
   );
 }
