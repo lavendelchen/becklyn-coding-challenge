856d727fd981ead13cb3ab70070ee253d1ea3895
Lavendelchen <lavendel02sh@gmail.com>
2024-09-29 23:18:22 +0200

    basic structure of jobfilter done

--- src/components/JobOverview/JobOverview.tsx
+++ src/components/JobOverview/JobOverview.tsx
@@ -1,31 +1,10 @@
 "use client";
 
 import styles from "./JobOverview.module.css";
-import { forwardRef } from "react";
 import { useQuery } from "@apollo/client";
 import { InitialJobsQuery, InitialJobsQueryVariables } from "@/types/graphql";
 import { INITIAL_JOBS } from "@/lib/query";
-import { Select } from "@mui/base/Select";
-import { Option } from "@mui/base/Option";
-
-type SelectRootSlotProps<TValue, Multiple> = {
-  ownerState: any;
-  children?: React.ReactNode;
-};
-
-const Button = forwardRef<HTMLButtonElement, SelectRootSlotProps<any, any>>(
-  function Button(props, ref) {
-    const { ownerState, ...other } = props;
-    return (
-      <button type="button" {...other} ref={ref}>
-        {other.children}
-        <span className="material-symbols-outlined"> 
-          keyboard_arrow_down
-        </span>
-      </button>
-    );
-  }
-);
+import JobFilter from "../JobSelect/JobFilter";
 
 export default function JobOverview() {
   const initialJobsVariables = {
@@ -64,26 +43,7 @@ export default function JobOverview() {
       <h1 className={styles.heading}>Hier beginnt deine Zukunft</h1>
       <div className={styles.filters}>
         {Array.from({ length: 3 }, (_, index) => (
-          <Select
-            key={index}
-            className={styles.select}
-            slots={{
-              root: Button
-            }}
-            slotProps={{
-              popup: { className: styles.selectPopup },
-              listbox: { className: styles.selectListbox },
-            }}
-            value="Hello"
-            onChange={(event, newValue) => {}}
-          >
-            <Option className={styles.selectOption} key="Hello" value="Hello">
-              Hello
-            </Option>
-            <Option className={styles.selectOption} key="Hello" value="Hello">
-              Hello
-            </Option>
-          </Select>
+          <JobFilter />
         ))}
       </div>
     </div>
---
+++ src/components/JobSelect/JobFilter.tsx
@@ -0,0 +1,57 @@
+import styles from "./JobFilter.module.css";
+import { forwardRef } from "react";
+import { 
+  Select
+} from "@mui/base/Select";
+import { Option } from "@mui/base/Option";
+
+type SelectRootSlotProps<TValue, Multiple> = {
+  ownerState: any;
+  children?: React.ReactNode;
+};
+
+const Button = forwardRef<HTMLButtonElement, SelectRootSlotProps<any, any>>(
+  function Button(props, ref) {
+    const { ownerState, ...other } = props;
+    return (
+      <button type="button" {...other} ref={ref}>
+        {other.children}
+        <span className="material-symbols-outlined"> 
+          keyboard_arrow_down
+        </span>
+      </button>
+    );
+  }
+);
+
+export default function JobFilter() {
+  return (
+    <Select
+      // defaultListboxOpen={index === 2 ? true : false}
+      key={3}
+      className={styles.select}
+      slots={{
+        root: Button
+      }}
+      slotProps={{
+        popup: {
+          className: styles.selectPopup,
+          disablePortal: true
+        },
+        listbox: { className: styles.selectListbox },
+      }}
+      value="Hello"
+      onChange={(event, newValue) => {}}
+    >
+      <Option className={styles.selectOption} key="Hello" value="Hello">
+        Hello
+      </Option>
+      <Option className={styles.selectOption} key="Bye" value="Bye">
+        Bye
+      </Option>
+      <Option className={styles.selectOption} key="Mönchengladbach" value="Mönchengladbach">
+        Mönchengladbach
+      </Option>
+    </Select>
+  )
+}
