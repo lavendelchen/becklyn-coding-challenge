89ea01e1b5867e4c59e4464d0acc210120dc639b
Lavendelchen <lavendel02sh@gmail.com>
2024-10-04 13:37:51 +0200

    outsource JobFilters to their own component

---
+++ src/components/JobFilters/JobFilters.tsx
@@ -0,0 +1,57 @@
+import styles from "./JobFilters.module.css"
+
+import { useQuery } from "@apollo/client";
+import {
+  FilterQuery,
+  FilterQueryVariables
+} from "@/types/graphqlGenerated"
+import { FILTER_QUERY } from "@/lib/query";
+import { FilterContent, getFilterContent } from "@/lib/utils";
+
+import JobFilter from "../JobFilters/JobFilter/JobFilter";
+
+export default function JobFilters() {
+  const filterQuery = useQuery<FilterQuery, FilterQueryVariables>(
+    FILTER_QUERY,
+    { variables: { where: { available: true } } }
+  )
+
+  if (filterQuery.error)
+    console.dir(filterQuery.error, { depth: null, color: true})
+
+  let filterContent: FilterContent = {
+    departments: new Set<string>(),
+    cities: new Set<string>(),
+    levels: new Set<string>()
+  };
+  let disableFilters = (
+    filterQuery.loading || filterQuery.error ?
+    true : false
+  );
+
+  if (filterQuery.data?.jobCollection) {
+    filterContent = getFilterContent(filterQuery.data?.jobCollection.items)
+    console.dir(filterContent, { depth: null, color: true}) ///
+  }
+
+  const determinePlaceholder = (filterName: string) => {
+    if (filterQuery.loading)
+      return "Laden...";
+    else if (filterQuery.error)
+      return "Error :(";
+    else if (filterQuery.data)
+      return filterName;
+    else
+      return "You should never see this 🚨"
+  }
+
+  return (
+    <div className={styles.filters}>
+      <JobFilter
+        set={filterContent.departments}
+        disabled={disableFilters}
+        placeholder={determinePlaceholder("Bereich")}
+      />
+    </div>
+  )
+}
--- src/components/JobOverview/JobOverview.tsx
+++ src/components/JobOverview/JobOverview.tsx
@@ -5,16 +5,13 @@ import styles from "./JobOverview.module.css";
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import {
-  FilterQuery,
-  FilterQueryVariables,
   JobsQuery,
   JobsQueryVariables
 } from "@/types/graphqlGenerated";
 import { JobItems } from "@/types/graphqlAdditional";
-import { FILTER_QUERY, JOBS_QUERY } from "@/lib/query";
-import { FilterContent, getFilterContent } from "@/lib/utils";
+import { JOBS_QUERY } from "@/lib/query";
 
-import JobFilter from "../JobFilter/JobFilter";
+import JobFilters from "../JobFilters/JobFilters";
 import JobCard from "../JobCard/JobCard";
 import Pagination from "../Pagination/Pagination"
 
@@ -40,33 +37,13 @@ export default function JobOverview() {
     },
   }
 
-  const jobsQuery = useQuery<JobsQuery, JobsQueryVariables>(JOBS_QUERY, {
-    variables: jobsVariables
-  })
+  const jobsQuery = useQuery<JobsQuery, JobsQueryVariables>(
+    JOBS_QUERY,
+    { variables: jobsVariables }
+  )
 
-  const filterQuery = useQuery<FilterQuery, FilterQueryVariables>(FILTER_QUERY, {
-    variables: { where: { available: true } }
-  })
-
-  if (filterQuery.error)
-    console.dir(filterQuery.error, { depth: null, color: true})
   if (jobsQuery.error)
     console.dir(jobsQuery.error, { depth: null, color: true})
-
-  let filterContent: FilterContent = {
-    departments: new Set<string>(),
-    cities: new Set<string>(),
-    levels: new Set<string>()
-  };
-  let disableFilters = (
-    filterQuery.loading || filterQuery.error ?
-    true : false
-  );
-
-  if (filterQuery.data?.jobCollection) {
-    filterContent = getFilterContent(filterQuery.data?.jobCollection.items)
-    console.dir(filterContent, { depth: null, color: true}) ///
-  }
 
   let jobCount = 0;
   let jobs: JobItems = [];
@@ -78,16 +55,6 @@ export default function JobOverview() {
     pagesCount = Math.ceil(jobCount / PAGINATION_LIMIT);
   }
 
-  const determinePlaceholder = (filterName: string) => {
-    if (filterQuery.loading)
-      return "Laden...";
-    else if (filterQuery.error)
-        return "Error :(";
-    else if (filterQuery.data)
-      return filterName;
-    else
-      return "You should never see this 🚨"
-  }
   const handlePageChange = (event: React.ChangeEvent<unknown>, newPage: number) => {
     setCurrentPage(newPage)
   }
@@ -97,13 +64,7 @@ export default function JobOverview() {
       <div className={styles.jobOverviewHeader}>
         <h5 className={styles.subHeading}>{jobCount} offene Stellen bei CreditPlus</h5>
         <h1 className={styles.heading}>Hier beginnt deine Zukunft</h1>
-        <div className={styles.filters}>
-          <JobFilter
-            set={filterContent.departments}
-            disabled={disableFilters}
-            placeholder={determinePlaceholder("Bereich")}
-          />
-        </div>
+        <JobFilters />
       </div>
       <div className={styles.jobOverviewBody}>
         <h2 className={styles.bodyHeading}>Aktuelle Jobangebote</h2>
